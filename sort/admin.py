7289283f1840a5c7997a2cb70075698baad913df
kexijiang <49815626+kexijiang@users.noreply.github.com>
2020-07-23 11:11:10 +0800

    开发第四天

--- sort/admin.py
+++ sort/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from sort.models import Sort
+from sort.models import Sort, Article_sort
 
 admin.site.register(Sort, admin.ModelAdmin)
+
+admin.site.register(Article_sort, admin.ModelAdmin)
